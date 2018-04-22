db4e9cf72554af6b43c51f22005eb5ba61877861
zhichao <zhichaoglory@gmail.com>
2018-04-22 14:40:45 +0800

    用户注册和登陆

---
+++ src/main/java/com/huifeng/pojo/UserEntity.java
@@ -0,0 +1,113 @@
+package com.huifeng.pojo;
+
+import java.util.Date;
+
+public class UserEntity{
+    private  int id;
+    private String loginName;//登录名
+    private String name;//用户名
+    private String nickName;//昵称
+    private String password;//密码
+    private String phone;//电话
+    private String IdCard;//身份证
+    private Date birthday;//生日
+    private int sex;//性别（0 男，1 女）
+
+    public UserEntity() {
+    }
+
+    public UserEntity(int id, String loginName, String name, String nickName, String password, String phone, String idCard, Date birthday, int sex) {
+        this.id = id;
+        this.loginName = loginName;
+        this.name = name;
+        this.nickName = nickName;
+        this.password = password;
+        this.phone = phone;
+        IdCard = idCard;
+        this.birthday = birthday;
+        this.sex = sex;
+    }
+
+    public UserEntity(String loginName, String name, String nickName, String password, String phone, String idCard, Date birthday, int sex) {
+        this.loginName = loginName;
+        this.name = name;
+        this.nickName = nickName;
+        this.password = password;
+        this.phone = phone;
+        IdCard = idCard;
+        this.birthday = birthday;
+        this.sex = sex;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getLoginName() {
+        return loginName;
+    }
+
+    public void setLoginName(String loginName) {
+        this.loginName = loginName;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getNickName() {
+        return nickName;
+    }
+
+    public void setNickName(String nickName) {
+        this.nickName = nickName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getIdCard() {
+        return IdCard;
+    }
+
+    public void setIdCard(String idCard) {
+        IdCard = idCard;
+    }
+
+    public Date getBirthday() {
+        return birthday;
+    }
+
+    public void setBirthday(Date birthday) {
+        this.birthday = birthday;
+    }
+
+    public int getSex() {
+        return sex;
+    }
+
+    public void setSex(int sex) {
+        this.sex = sex;
+    }
+}
---
+++ src/main/webapp/index.jsp
@@ -0,0 +1,20 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: ASUS
+  Date: 2018/4/22
+  Time: 14:22
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>首页</title>
+</head>
+<body>
+<form action="#">
+    <input type="text" name="loginName" placeholder="登录名">
+    <input type="password" name="password" placeholder="密码">
+    <input type="submit" value="登陆">
+</form>
+</body>
+</html>
